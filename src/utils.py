aa74e00a463f5811f7f48de258d990932eb0dade
Suguna Sai Navaneeth Rentala <118918735+rentala-navaneeth@users.noreply.github.com>
2026-06-03 22:15:03 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,2 @@
-# ============================================================
-# utils.py
-# Utility functions for behavioral cloning project
-# ============================================================
-
 import cv2
 import numpy as np
